4e99000563097611aacecb89bed002b138657a20
RahmanMd. Rezaur <reza@RahmanMds-MacBook-Air.local>
2016-04-26 13:32:15 +0600

    init

---
+++ server.js
@@ -0,0 +1,31 @@
+var http = require('http');
+var port = 3000;
+
+http.createServer(function(req, res) {
+      res.writeHead(200, {
+            'Content-Type': 'text/plain'
+      });
+
+
+      var Botkit = require('botkit');
+    var controller = Botkit.slackbot();
+    var bot = controller.spawn({
+    token: 'xoxb-37740112834-cp4LAjC3p5hGQZKQarY0kQQ5'
+    })
+    bot.startRTM(function(err,bot,payload) {
+    if (err) {
+            throw new Error('Could not connect to Slack');
+    }
+    });
+
+    controller.hears(["world","^pattern$"],["direct_message","direct_mention","mention","ambient"],function(bot,message) {
+  // do something to respond to message
+  // all of the fields available in a normal Slack message object are available
+  // https://api.slack.com/events/message
+  bot.reply(message,'You used a keyword!');
+  bot.say("howdy")
+});
+
+
+      res.end("Hello world");
+}).listen(port);
